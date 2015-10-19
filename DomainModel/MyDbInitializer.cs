b538f1458fb70518c0e8663db3f8cac5a4d576d4
Rik van den Heuvel <rheuvel6@student.avans.nl>
2015-10-19 10:30:05 +0200

    Initial implementation of reposiroty pattern

---
+++ DomainModel/MyDbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class MyDbInitializer : DropCreateDatabaseIfModelChanges<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+                        
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DomainModel/Repositories/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel.Interfaces;
+using DomainModel.Model;
+
+namespace DomainModel.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public Category Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
